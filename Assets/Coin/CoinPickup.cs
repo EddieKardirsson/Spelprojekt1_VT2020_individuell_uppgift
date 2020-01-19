5e46c1721cdafbc21d12100e2eb429f20ae58343
EddieKardirsson <ed.kardirsson@hotmail.com>
2020-01-19 23:09:44 +0100

    Coin-object created, C#-script added to Coin and Coin is prefabbed

---
+++ Assets/Coin/CoinPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    public Transform coinMesh;
+
+    private void OnTriggerEnter(Collider other){
+        if (other.gameObject.tag == "Player" && coinMesh.gameObject.activeInHierarchy){
+            GameLogic.CoinPickup();
+            coinMesh.gameObject.SetActive(false);
+        }
+    }
+
+
+}
