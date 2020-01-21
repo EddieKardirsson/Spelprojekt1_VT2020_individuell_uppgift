1a03b4a456d4cbe671118fdee113a4b0953ae8a4
EddieKardirsson <ed.kardirsson@hotmail.com>
2020-01-21 17:39:24 +0100

    Win/Lose-screens added

---
+++ Assets/4.WinLose/LevelExit.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+
+    public Canvas loadCanvas;
+    public string nextScene;
+
+    private readonly float delayTime = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(LoadNextScene());
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(LoadNextScene());
+        }
+    }
+
+    private IEnumerator LoadNextScene(){
+        Instantiate(loadCanvas);
+        yield return new WaitForSecondsRealtime(delayTime);
+        SceneManager.LoadScene(nextScene);
+    }
+}
