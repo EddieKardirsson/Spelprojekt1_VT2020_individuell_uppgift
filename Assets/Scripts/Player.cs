3ba83e274c9af84f882621136ea9e3bb89c4e3b3
EddieKardirsson <ed.kardirsson@hotmail.com>
2020-01-21 08:50:23 +0100

    NPC, Fungus-plugin, conversation and bridge added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Player : MonoBehaviour
+{
+    public void DisableMovement(){
+        gameObject.GetComponent<ThirdPersonCharacter>().enabled = false;
+        gameObject.GetComponent<ThirdPersonUserControl>().enabled = false;
+    }
+
+    public void EnableMovement(){
+        gameObject.GetComponent<ThirdPersonCharacter>().enabled = true;
+        gameObject.GetComponent<ThirdPersonUserControl>().enabled = true;
+    }
+}
