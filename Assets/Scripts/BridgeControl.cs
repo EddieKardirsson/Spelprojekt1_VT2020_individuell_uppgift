852231d0814a9a8637f11e141d0db1f98409abcc
EddieKardirsson <ed.kardirsson@hotmail.com>
2020-01-21 22:23:09 +0100

    Bridge animations created and set

---
+++ Assets/Scripts/BridgeControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeControl : MonoBehaviour
+{
+    Animator animator;
+    
+    void Start(){
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetBridgeDown(string name){
+        animator.SetTrigger(name);
+    }    
+}
